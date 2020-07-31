ecf6aa5e25affa7c4f7e900b6aba66ee1ae92339
Jody Eggleston <jmeggles@gmail.com>
2020-07-31 11:45:20 -0400

    added notes

--- models/Workout.js
+++ models/Workout.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
+    default: Date.now
   },
   exercise: [
     {
       // - type (type -> String)
       type: {
         type: String,
-        required: true
+        required: true,
+        trim: true
       },
       // - name (type -> String)
       name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
       },
       // - duration (type -> Number)
       duration: {
         type: Number,
-        required: true
+        required: true,
+        trim: true
       },
       // - weight (type -> Number)
       weight: {
@@ -43,6 +46,12 @@ const WorkoutSchema = new Schema({
   ]
 });
 
+WorkoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
 const Workout = mongoose.model("Workout", WorkoutSchema);
-// export Workout module (not libraries collection)
+// export Workout module 
 module.exports = Workout;
--- routes/apiRoute/workoutRoute.js
+++ routes/apiRoute/workoutRoute.js
@@ -9,7 +9,6 @@
 
 const router = require('express').Router();
 
-
 // /api/workouts
 router.get("/", function(req, res){
 
@@ -17,7 +16,13 @@ router.get("/", function(req, res){
 
 // /api/workouts/range
 router.get("/range", function(req, res){
-
+    db.Workout.find({}).limit(5)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 })
 
 module.exports = router;
@@ -25,21 +30,58 @@ module.exports = router;
 
     // - GET ( "/api/workouts" ) - Get all workouts
         // - You will also need to figure out how to calculate the total duration from all exercises inside the exercises Array.
-
+    // app.post("/api/workouts", (req, res) => {
+    //     db.Workout.create({})
+    //     .then(dbWorkout => {
+    //       res.json(dbWorkout);
+    //     })
+    //     .catch(err => {
+    //       res.json(err);
+    //     });
+    // });
 
 
 
 
     // - GET ( "/api/workouts/range" ) - Get the first 7 Workouts from the Workout table to be used in the "stats" page.
-
+    // app.get("/api/workouts/range", function(req, res) {
+    //     db.Workout.find({}).limit(7)
+    //       .then(dbWorkout => {
+    //         res.json(dbWorkout);
+    //       })
+    //       .catch(err => {
+    //         res.json(err);
+    //       });
+    //   });
 
     
 
 
     // - POST ( "/api/workouts" ) - Create a new Workout
-
+    // app.post("/api/workouts", (req, res) => {
+    //     db.Workout.create({})
+    //       .then(dbWorkout => {
+    //         res.json(dbWorkout);
+    //       })
+    //       .catch(err => {
+    //         res.json(err);
+    //       });
+    //   });
 
 
 
 
     // - PUT ( "/api/workouts/:id" ) - Update a Workout
+    // app.put("/api/workouts/:id", function({body, params}, res) {
+    //     db.Workout.findByIdAndUpdate(
+    //       params.id,
+    //       { $push: { exercises: body } },
+    //       { new: true }
+    //     )
+    //       .then(dbWorkout => {
+    //         res.json(dbWorkout);
+    //       })
+    //       .catch(err => {
+    //         res.json(err);
+    //       });
+    //   });
